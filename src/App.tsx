6392159fb72729ca62f5d0db8df2f95b190d36b5
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2024-10-02 18:26:48 +0530

    fixed size issue

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ const App = () => {
     <div>
       <Header />
       <div className="flex justify-center items-center h-screen relative">
-        <div className="bg-black h-screen w-full overflow-hidden absolute">
+        <div className="bg-[#000814] h-screen w-full overflow-hidden absolute">
           <SparklesCore className=" w-full h-[200vh] z-[1] translate-y-[-50%] absolute"  particleColor="#fff" particleSize={1} speed={1} particleDensity={20} maxSize={0.2} background="transparent"
           />
           <div className="h-[1460vh] z-20 w-[100vh] absolute left-[50%] translate-x-[-50%] top-[100%] blur-[200px] opacity-100 bg-white rounded-full aspect-square flex justify-center items-center">
           </div>
             <div className="h-[510vh] w-[500vh] absolute left-[50%] translate-x-[-50%] top-[80%] z-[20] bg-white rounded-full aspect-[1/1] flex justify-center items-center">
-              <div className="h-[510vh] w-[511vh] cursor-pointer absolute top-[0.05%] bg-black rounded-full aspect-[1/1] blur-[1.5px]">
+              <div className="h-[510vh] w-[511vh] cursor-pointer absolute top-[0.05%] bg-[#000814] rounded-full aspect-[1/1] blur-[1.5px]">
               </div>
             </div>
           <div className="blob1 z-[19]"></div>
