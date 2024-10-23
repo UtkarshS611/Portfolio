f48bc34232643f6b781af5336dd6e0d523209dba
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2024-10-23 19:44:23 +0530

    added links and fixed bugs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="text-white py-4 mt-6 z-[900] absolute left-[50%] translate-x-[-50%] w-[80%] flex  items-center justify-between">
             <div className="w-full">
-                Logo
+                Utkarsh
             </div>
             <div className="w-full hidden md:flex justify-center items-center gap-4 bg-[#ffffff16] backdrop-blur-[200px] py-1 rounded-full border-[0.8px] border-[#ffffff4c]">
                 <a href="" className="hover:bg-[#00000096] duration-200 hover:backdrop-blur-[200px] px-4 py-2 rounded-full">About</a>
@@ -12,14 +12,14 @@ const Header = () => {
                 <a href="" className="hover:bg-[#00000096] duration-200 hover:backdrop-blur-[200px] px-4 py-2 rounded-full">Skills</a>
             </div>
             <div className="flex gap-4 w-full justify-end">
-                <a href="https://www.linkedin.com/in/utkarsh-singh-5630b42aa/" className="hidden md:flex cursor-pointer gap-2 font-light justify-center items-center">
+                <a href="https://www.linkedin.com/in/utkarsh-singh-5630b42aa/" target="_blank" className="hidden md:flex cursor-pointer gap-2 font-light justify-center items-center">
                     Linkedin
                     <IoIosArrowRoundForward className="rotate-[-45deg] text-2xl" />
                 </a>
-                <button className="flex gap-2 font-light justify-center items-center">
+                <a href="https://drive.google.com/file/d/1q25HSTjPWP8skAQ9JE5f9RTdrcS7B3ND/view" target="_blank" className="flex gap-2 font-light justify-center items-center">
                     Resume
                     <IoIosArrowRoundForward className="rotate-[-45deg] text-2xl"/>
-                </button>
+                </a>
             </div>
         </div>
     )
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,20 +1,18 @@
-import ProjectShowCase from "../ProjectShowCase/ProjectShowCase"
+import ProjectShowCase from "../ProjectShowCase/ProjectShowCase";
 
 const Projects = () => {
-    return (
-        <div className="mt-[5rem] md:mt-[8rem]">
-            <div className="uppercase text-sm opacity-80 text-center">
-                Featured work
-            </div>
-            <div className="flex gap-4 font-semibold items-end justify-center md:mt-[1.5rem] mt-[1rem]">
-                <span className="text-4xl">Curated</span>
-                <span className="text-xl font-Accent"> 
-                    Work
-                </span>
-            </div>
-            <ProjectShowCase/>
-        </div>
-    )
-}
+  return (
+    <div className="mt-[5rem] md:mt-[8rem]">
+      <div className="uppercase text-sm opacity-80 text-center">
+        Featured work
+      </div>
+      <div className="flex gap-4 font-semibold items-end justify-center md:mt-[1.5rem] mt-[1rem]">
+        <span className="text-4xl">Curated</span>
+        <span className="text-xl font-Accent">Work</span>
+      </div>
+      <ProjectShowCase />
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
