d802f0f18a81943d747d1f7cf3eb7212e1e4e368
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2024-12-11 14:51:08 +0530

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import Home from "../src/Components/Home/Home";
-import ProjectSection  from "../src/Components/Projects/ProjectSection";
-import Footer from "../src/Components/Footer/Footer";
+// import Home from "../src/Components/Home/Home";
+// import ProjectSection  from "../src/Components/Projects/ProjectSection";
+// import Footer from "../src/Components/Footer/Footer";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import ProjectSection from "./Components/Projects/ProjectSection";
 import { ThemeProvider } from "./Contexts/ThemeContext";
 
 const App = () => {
   return (
     <ThemeProvider>
       <main className="bg-white dark:bg-[#000814]">
-        <Home />
-        <ProjectSection />
-        <Footer />
+        {/* <Home /> */}
+        <Home/>
+        <ProjectSection/>
+        <Footer/>
+        {/* <ProjectSection /> */}
+        {/* <Footer /> */}
       </main>
     </ThemeProvider>
   );
